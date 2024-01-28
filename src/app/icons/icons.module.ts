869b48b51d2a3ac5328bf632d0602d6a7cb69edd
farada <faradaym1leena@mail.ru>
2024-01-28 23:16:07 +0300

    app(feat) yey вспомнила что иконки сначала надо заимпортить , ток потом использовать

--- src/app/icons/icons.module.ts
+++ src/app/icons/icons.module.ts
@@ -1,12 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {TablerIconsModule} from 'angular-tabler-icons';
-import {IconCamera, IconHeart, IconBrandGithub} from 'angular-tabler-icons/icons';
+import {IconCamera, IconHeart, IconBrandGithub, IconCup} from 'angular-tabler-icons/icons';
 
 const icons = {
   IconCamera,
   IconHeart,
-  IconBrandGithub
+  IconBrandGithub,
+  IconCup
 };
 
 @NgModule({
